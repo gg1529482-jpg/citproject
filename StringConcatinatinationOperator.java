44bb1b9cecb8960e59bc3e660f1d4e09cee7f4ae
gg1529482-jpg <gg1529482@gmail.com>
2026-03-26 10:58:01 +0530

    Updated unitsconsumption

--- StringConcatinatinationOperator.java
+++
@@ -1,7 +0,0 @@
-public class StringConcatinatinationOperator {
-    public static void main(String[] args) {
-        String name="XXX";
-        int age =25;
-        System.out.println("Name: " +name+ "Age: " +age);
-    }
-}
