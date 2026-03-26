40f4ca58bf7480b958417fc7971385fcec7d2f6e
gg1529482-jpg <gg1529482@gmail.com>
2026-03-26 11:21:48 +0530

    Updated loansanction

---
+++ LoanSanctionOperator.java
@@ -0,0 +1,13 @@
+public class LoanSanctionOperator {
+    public static void main(String[] args) {
+        String name = "geetha";
+        int age=21;
+        double salary = 18000;
+        if(age>=21)
+           System.out.println("loan sanction");
+        else
+           System.out.println("loan unsanction");
+
+    }
+    
+}
